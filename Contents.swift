a93622958e2f983a61f781a1ad05b98adafea4c8
Edwin Cardenas <ecardenas@lwolf.com>
2025-03-13 20:12:26 -0500

    feat: int type and string interpolation

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,11 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+var numberOfSpotlights = 4
+var population: Int
+
+population = 5422
+
+let townName: String = "Springfield"
+let townDescription = "\(townName) has a population of \(population) and \(numberOfSpotlights) spotlights."
+
+print(townDescription)
